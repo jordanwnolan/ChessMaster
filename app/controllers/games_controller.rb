1973cc73b0509de169e1a152cd0668f7496d478f
Jordan Nolan <jordanwn@gmail.com>
2014-05-08 12:34:14 -0400

    can make moves, only binds listeners to current players pieces

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,19 +5,26 @@ class GamesController < ApplicationController
       player_2_id: Player.create({username: 'player2'}).id,
       board: Board.new})
     # fail
+    @turn = "white"
     @game.save
   end
 
   def make_move
     @game = Game.find(params[:game])
-    # debugger
-    # fail
+
     color = params[:color] == 'white' ? :white : :black
     from_pos = params[:from].map(&:to_i)
     to_pos = params[:to].map(&:to_i);
+    begin
+      @game.board.move(from_pos, to_pos, color)
+      @game.save
+      @turn = params[:turn] == 'white' ? 'black' : 'white'
+    rescue Exception => e
+      @errors = [e.message]
+      @game = Game.find(params[:game])
+      @turn = params[:turn]
+    end
 
-    @game.board.move(from_pos, to_pos, color)
-    @game.save
     render partial: 'games/board', locals: { game: @game }
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,5 @@
 class Game < ActiveRecord::Base
   require 'all_pieces'
-  # require 'yaml'
 
   serialize :board
   attr_accessor :game_board
